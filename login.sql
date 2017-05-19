ca0f6c66c12e94f1ba270a1c14d2e29ae653036c
semitro <semitro_8@mail.ru>
2017-05-19 23:00:19 +0300

    First commit

---
+++ login.sql
@@ -0,0 +1,101 @@
+COLUMN "АББРЕВИАТУРА"               FORMAT A20 WOR
+COLUMN "АУДИТОРИЯ"                  FORMAT A20 WOR
+COLUMN "АУДИТОРИЯ_К"                FORMAT A20 WOR
+COLUMN "В_СВЯЗИ_С"                  FORMAT A20 WOR
+COLUMN "ВЕД_ИД"                     FORMAT 999999
+COLUMN "ВИД_ОБУЧ_ИД"                FORMAT 999999
+COLUMN "ВМЕСТО"                     FORMAT A20 WOR
+COLUMN "ВР_ИД"                      FORMAT 999999
+COLUMN "ВРЕМЯ"                      FORMAT A20 WOR
+COLUMN "ВРЕМЯ_К"                    FORMAT A20 WOR
+COLUMN "ГРУППА"                     FORMAT A6  WOR
+COLUMN "ДАТА"                       FORMAT A10 WOR
+COLUMN "ДАТА_ГОС"                   FORMAT A10 WOR
+COLUMN "ДАТА_К"                     FORMAT A10 WOR
+COLUMN "ДАТА_ЛИКВИДАЦИИ"            FORMAT A10 WOR
+COLUMN "ДАТА_РОЖДЕНИЯ"              FORMAT A10 WOR
+COLUMN "ДАТА_СМЕРТИ"                FORMAT A10 WOR
+COLUMN "ДАТА_СОЗДАНИЯ"              FORMAT A10 WOR
+COLUMN "ДАТА_УТВЕРЖДЕНИЯ"           FORMAT A10 WOR
+COLUMN "ДИС_ИД"                     FORMAT 999999
+COLUMN "ЕДИНИЦА_ИЗМЕРЕНИЯ"          FORMAT A20 WOR
+COLUMN "ЗНАЧЕНИЕ"                   FORMAT A20 WOR
+COLUMN "ИД"                         FORMAT 999999
+COLUMN "ИМЯ"                        FORMAT A20 WOR
+COLUMN "ИМЯ_В_ВИН_ПАДЕЖЕ"           FORMAT A20 WOR
+COLUMN "ИМЯ_В_ДАТ_ПАДЕЖЕ"           FORMAT A20 WOR
+COLUMN "ИМЯ_В_ИМИН_ПАДЕЖЕ"          FORMAT A20 WOR
+COLUMN "ИМЯ_В_ПРЕД_ПАДЕЖЕ"          FORMAT A20 WOR
+COLUMN "ИМЯ_В_РОД_ПАДЕЖЕ"           FORMAT A20 WOR
+COLUMN "ИМЯ_В_ТВОР_ПАДЕЖЕ"          FORMAT A20 WOR
+COLUMN "ИНН"                        FORMAT A20 WOR
+COLUMN "ИНОСТРАН"                   FORMAT A20 WOR
+COLUMN "КВАЛ_ИД"                    FORMAT 999999
+COLUMN "КОГДА_ИЗМЕНИЛ"              FORMAT A10 WOR
+COLUMN "КОГДА_СОЗДАЛ"               FORMAT A10 WOR
+COLUMN "КОД"                        FORMAT A20 WOR
+COLUMN "КОД_НАПРСПЕЦ"               FORMAT A6 WOR
+COLUMN "КОМ_ИД"                     FORMAT 999999
+COLUMN "КОНЕЦ"                      FORMAT A10 WOR
+COLUMN "КОНЕЦ_ПО_ПРИКАЗУ"           FORMAT A10 WOR
+COLUMN "КОРОТКОЕ_ИМЯ"               FORMAT A20 WOR
+COLUMN "КТО_ИЗМЕНИЛ"                FORMAT A10 WOR
+COLUMN "КТО_СОЗДАЛ"                 FORMAT A10 WOR
+COLUMN "КУРС"                       FORMAT A4 WOR
+COLUMN "МЕСТО_РОЖДЕНИЯ"             FORMAT A20 WOR
+COLUMN "НАИМЕНОВАНИЕ"               FORMAT A20 WOR
+COLUMN "НАПС_ИД"                    FORMAT 999999
+COLUMN "НАЧАЛО"                     FORMAT A10 WOR
+COLUMN "НЕДЕЛЬ"                     FORMAT A6 WOR
+COLUMN "НЗК"                        FORMAT A6 WOR
+COLUMN "НОМЕР"                      FORMAT A6 WOR
+COLUMN "НОМЕР_В_ЦИКЛЕ"              FORMAT A13 WOR
+COLUMN "НОМЕР_ДИСЦИПЛИНЫ_ПО_ВЫБОРУ" FORMAT A20 WOR
+COLUMN "НОМЕР_ДОКУМЕНТА"            FORMAT A15 WOR
+COLUMN "НОМЕР_КОНТРОЛЯ"             FORMAT A14 WOR
+COLUMN "НОМЕР_СЕМЕСТРА"             FORMAT A13 WOR
+COLUMN "НС_ИД"                      FORMAT 999999
+COLUMN "ОБЪЕМ"                      FORMAT 99999
+COLUMN "ОТД_ИД"                     FORMAT 999999
+COLUMN "ОТД_ИД_ЗАКРЕПЛЕН_ЗА"        FORMAT 999999
+COLUMN "ОТЧЕСТВО"                   FORMAT A20 WOR
+COLUMN "ОЦЕНКА"                     FORMAT A6 WOR
+COLUMN "П_ПРКОК_ИД"                 FORMAT 999999
+COLUMN "ПИН"                        FORMAT A12 WOR
+COLUMN "ПЛАН_ИД"                    FORMAT 999999
+COLUMN "ПЛАН_ИД_ОСНОВ_НА"           FORMAT 999999
+COLUMN "ПЛАН_ПРИЕМА_БЮДЖЕТ"         FORMAT A17 WOR
+COLUMN "ПЛАН_ПРИЕМА_ПРОЧИЙ"         FORMAT A17 WOR
+COLUMN "ПОЛ"                        FORMAT A3 WOR
+COLUMN "ПОЛУПРОХОДНОЙ_БАЛЛ"         FORMAT A18 WOR
+COLUMN "ПОРЯДОК"                    FORMAT A7  WOR
+COLUMN "ПРЕПОДАВАТЕЛЬ"              FORMAT 999999
+COLUMN "ПРИЗНАК"                    FORMAT A7 WOR
+COLUMN "ПРИМЕЧАНИЕ"                 FORMAT A20 WOR
+COLUMN "ПРОХОДНОЙ_БАЛЛ"             FORMAT A14 WOR
+COLUMN "СВОТД_ИД"                   FORMAT 999999
+COLUMN "СВР_ИД"                     FORMAT 999999
+COLUMN "СЕМЕСТР"                    FORMAT A20 WOR
+COLUMN "СОРТ"                       FORMAT A20 WOR
+COLUMN "СОСТОЯНИЕ"                  FORMAT A9 WOR
+COLUMN "СПЕЦИАЛЬНЫЙ_ПРОХОДНОЙ_БАЛЛ" FORMAT A20 WOR
+COLUMN "СПЛ_ИД"                     FORMAT 999999
+COLUMN "СРОК_ОБУЧЕНИЯ"              FORMAT A12 WOR
+COLUMN "СРОК_СДАЧИ"                 FORMAT A10 WOR
+COLUMN "СЭС_ИД"                     FORMAT 999999
+COLUMN "ТВ_ИД"                      FORMAT 999999
+COLUMN "ТЕКСТ"                      FORMAT A20 WOR
+COLUMN "ТПЛ_ИД"                     FORMAT 999999
+COLUMN "ТС_ИД"                      FORMAT 999999
+COLUMN "УРОВЕНЬ"                    FORMAT 999999
+COLUMN "УЧГОД"                      FORMAT A9 WOR
+COLUMN "УЧЕБНЫЙ_ГОД"                FORMAT A9 WOR
+COLUMN "ФАМИЛИЯ"                    FORMAT A20 WOR
+COLUMN "ФО_ИД"                      FORMAT 999999
+COLUMN "ЦД_ИД"                      FORMAT 999999
+COLUMN "ЧЛВК_ИД"                    FORMAT 999999
+COLUMN "ЭСТ_ИД"                     FORMAT 999999
+SET linesize 2000
+SET pagesize 60
+SEt linesize 80
+ALTER SESSION SET nls_date_format='dd.mm.yyyy';
