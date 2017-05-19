45955b33a408dbe123abd7edf7468db632c98b70
trinistr <trinistr@gmail.com>
2017-05-20 00:35:35 +0300

    Добавлена getBandMembers()

--- scripts/functions.sql
+++ scripts/functions.sql
@@ -25,3 +25,14 @@ Begin
 End 
 $$
 language plpgsql;
+
+create or replace function getBandMembers (bandName varchar(80), since date default null, until date default null)
+	returns table(name varchar(80), join_date date, leave_date date, role varchar(80))
+	language SQL STABLE
+as $$
+	select Person.name, Member.join_date, Member.leave_date, Role.name
+	 from Band join Member using(band_id) join Person using(person_id) join Member_Role using(member_id) join Role using(role_id)
+	 where Band.name=bandName
+	 and (Member.join_date is null or until is null or Member.join_date<until)
+	 and (Member.leave_date is null or since is null or Member.leave_date>since);
+$$;
