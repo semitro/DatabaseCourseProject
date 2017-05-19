82130650490afdde4c5b65175b5f711e184534f1
semitro <semitro_8@mail.ru>
2017-05-20 00:33:09 +0300

    -

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -1,2 +1,12 @@
+create or replace function TriggerAliasDateCorrect() 
+	returns trigger As '
+Begin
+ if (new.start_date < (select birth_date from Alias join person using(person_id)))
+  then return NULL;
+ end if;
+End;' language plpgsql;
 
+create trigger TriggerAliasDateCorrect
+musicalgroups-# before insert or update on Alias
+musicalgroups-# execute procedure TriggerAliasDateCorrect()
 
