3deafc5e99f9faa956e796e44f42157cd7340d67
trinistr <trinistr@gmail.com>
2017-05-22 23:04:44 +0300

    Добавлена getPersonalHistory()

--- scripts/functions.sql
+++ scripts/functions.sql
@@ -1,7 +1,8 @@
 create or replace function getAllCompositionsBy (bandName varchar(80))
 	returns table(id integer,name varchar(80),creation_date Date, Style varchar(80))
-as
-$$
+	language plpgsql
+	STABLE
+as $$
 begin
 	return Query 
 	select distinct Composition.composition_id, Composition.name, Composition.creation_date, Style.name Style 
@@ -10,11 +11,12 @@ begin
 		 join composition using(composition_id) join Style using(style_id) 
 	where Band.name = bandName order by Composition.creation_date;
 end
-$$
-language plpgsql;
+$$;
 
 create or replace function getAllCompositionsWrittenBy (person_name varchar(80))
 	returns table(id integer, name varchar(80),creation_date Date, Style varchar(80), of_what varchar(80))
+	language plpgsql
+	STABLE
 as $$
 Begin
 	return Query	
@@ -23,12 +25,12 @@ Begin
 	Person join Author using(person_id) join Composition using(composition_id) join Style using(style_id)
 	where Person.name = person_name;
 End 
-$$
-language plpgsql;
+$$;
 
 create or replace function getBandMembers (bandName varchar(80), since date default null, until date default null)
 	returns table(name varchar(80), join_date date, leave_date date, role varchar(80))
-	language SQL STABLE
+	language SQL
+	STABLE
 as $$
 	select Person.name, Member.join_date, Member.leave_date, Role.name
 	 from Band join Member using(band_id) join Person using(person_id) join Member_Role using(member_id) join Role using(role_id)
@@ -39,9 +41,10 @@ $$;
 
 -- Даты ограничивают релиз альбома, не запись
 create or replace function getBandAlbums (bandName varchar(80), since date default null, until date default null)
-	returns table(name varchar(80), is_single boolean, released date, label varchar(80), copies int,
+	returns table (name varchar(80), is_single boolean, released date, label varchar(80), copies int,
 		rec_from date, rec_to date, studios varchar(160), collab varchar)
-	language SQL STABLE
+	language SQL
+	STABLE
 as $$
 	with Collab as (select album_id, string_agg(B2.name,', ') as collab
 		from (Band as B1 join Album_Band as A1 using(band_id)) join (Band as B2 join Album_Band as A2 using(band_id)) using(album_id)
@@ -55,12 +58,13 @@ as $$
 	 and (Album.release_date is null or until is null or Album.release_date<until);
 $$;
 
--- Имя можно не указывать, чтобы получить по всем
--- Дополнительно можно указать группу, если есть тёзки, чтобы сузить поиск
+-- Отбор можно производить по имени человека, по имени группы или по тому и другому сразу,
+-- или же можно вывести информацию по всем
 create or replace function getPersonalInfo (personName varchar(80) default null, bandName varchar(80) default null)
-	returns table(name varchar(80), aliases varchar, sex char(1), date_of_birth date, 
+	returns table (name varchar(80), aliases varchar, sex char(1), date_of_birth date, 
 		place_of_birth varchar, date_of_death date, place_of_death varchar)
-	language SQL STABLE
+	language SQL
+	STABLE
 as $$
 	with Aliases as (select person_id, string_agg(alias, ',') as aliases from Alias group by person_id)
 	select Person.name, Aliases.aliases, Person.sex, Person.birth_date, concat_ws(', ',P1.name,P1.addr,P1.country),
@@ -70,3 +74,18 @@ as $$
 	where (personName is null or Person.name=personName) and (bandName is null or person_id in
 		(select person_id from Member join Band using(band_id) where Band.name=bandName));
 $$;
+
+-- Отбор можно производить по имени человека, по имени группы или по тому и другому сразу,
+-- или же можно вывести информацию по всем
+create or replace function getPersonalHistory (personName varchar(80) default null, bandName varchar(80) default null)
+	returns table (name varchar(80), band varchar(80), member_from date, member_until date, role varchar(80),
+		role_from date, role_until date)
+	language SQL
+	STABLE
+as $$
+	select Person.name, Band.name, Member.join_date, Member.leave_date, Role.name, Member_Role.start_date, Member_Role.end_date
+	from Person left join Member using(person_id) join Band using(band_id)
+		left join Member_Role using(member_id) join Role using(role_id)
+	where (personName is null or Person.name=personName) and (bandName is null or person_id in
+		(select person_id from Member join Band using(band_id) where Band.name=bandName));
+$$
