a765b7217e4e38b4118374e5c77522991d8224b6
trinistr <trinistr@gmail.com>
2017-05-23 22:26:35 +0300

    Исправлен триггер

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -1,11 +1,11 @@
 create or replace function TriggerAliasDateCorrect() 
-	returns trigger As '
+	returns trigger As $$
 Begin
  if (new.start_date < (select birth_date from person where new.person_id = Person.person_id ) )
   then return NULL;
  end if;
 return new;
-End;' language plpgsql;
+End;$$ language plpgsql;
 
 create trigger TriggerAliasDateCorrect
 	before insert or update on Alias 
@@ -14,21 +14,29 @@ create trigger TriggerAliasDateCorrect
 
 
 create or replace function memberDateCorrect()
-	returns trigger As'
+	returns trigger As $$
+Declare
+	birth date;
+	death date;
+	formation date;
+	disbandment date;
 Begin
-  if( 
-	(select Person.birth_date from Person where Person.person_id = new.person_id) 
-	> 
-	(select disband_date from Band where Band.band_id = new.band_id)
-  ) then return null;
-  if( 
-	(select Person.death_date from Person where Person.person_id = new.person_id) 
-	<
-	(select formation_date from Band where Band.band_id = new.band_id)
-  ) then return null;
-end if;
-return new;
-End;'
+	birth := (select Person.birth_date from Person where Person.person_id = new.person_id);
+	death := (select Person.death_date from Person where Person.person_id = new.person_id);
+	formation := (select formation_date from Band where Band.band_id = new.band_id);
+	disbandment := (select disbandment_date from Band where Band.band_id = new.band_id);
+  	if birth>=disbandment or death<formation then
+  		return null;
+  	end if;
+  	if new.join_date not between birth and death
+  			or new.join_date not between formation and disbandment
+  			or new.leave_date not between birth and death
+  			or new.leave_date not between formation and disbandment then
+  		return null;
+	end if;
+	return new;
+End;
+$$
 language plpgsql;
 
 create trigger TriggerMemberDatesCorrect
