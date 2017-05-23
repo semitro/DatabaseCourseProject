07ffaf2741b37c92581f325b3e535d30f402c52a
trinistr <trinistr@gmail.com>
2017-05-24 01:15:39 +0300

    Функции приведены к единообразному виду, изменены названия

--- scripts/functions.sql
+++ scripts/functions.sql
@@ -1,7 +1,8 @@
-create or replace function getAllCompositionsBy (bandName varchar(80))
-	returns table(id integer,name varchar(80),creation_date Date, Style varchar(80))
-as
-$$
+create or replace function getCompositionsByBand (bandName varchar(80))
+	returns table(id integer,name varchar(80),creation_date date, Style varchar(80))
+	language plpgsql
+	stable
+as $$
 begin
 	return Query 
 	select distinct Composition.composition_id, Composition.name, Composition.creation_date, Style.name Style 
@@ -9,11 +10,12 @@ begin
 		join Album using(album_id) join Album_Band using(album_id) join Band using(band_id)
 	where Band.name = bandName order by Composition.creation_date;
 end
-$$
-language plpgsql;
+$$;
 
-create or replace function getAllCompositionsWrittenBy (person_name varchar(80))
-	returns table(id integer, name varchar(80),creation_date Date, Style varchar(80), of_what varchar(80))
+create or replace function getCompositionsAuthoredByPerson (person_name varchar(80))
+	returns table(id integer, name varchar(80),creation_date date, style varchar(80), of_what varchar(80))
+	language plpgsql
+	stable
 as $$
 Begin
 	return Query	
@@ -21,14 +23,14 @@ Begin
 	from Person join Author using(person_id) join Composition using(composition_id) left join Style using(style_id)
 	where Person.name = person_name;
 End 
-$$
-language plpgsql;
+$$;
 
-create or replace function getBandMembers (bandName varchar(80), since date default null, until date default null)
-	returns table(name varchar(80), join_date date, leave_date date, role varchar(80))
-	language SQL STABLE
+create or replace function getMembersOfBand (bandName varchar(80), since date default null, until date default null)
+	returns table(person_id int, name varchar(80), join_date date, leave_date date, role varchar(80))
+	language SQL
+	stable
 as $$
-	select Person.name, Member.join_date, Member.leave_date, Role.name
+	select Person.id, Person.name, Member.join_date, Member.leave_date, Role.name
 	 from Band join Member using(band_id) join Person using(person_id) join Member_Role using(member_id) join Role using(role_id)
 	 where Band.name=bandName
 	 and (Member.join_date is null or until is null or Member.join_date<until)
@@ -36,15 +38,16 @@ as $$
 $$;
 
 -- Даты ограничивают релиз альбома, не запись
-create or replace function getBandAlbums (bandName varchar(80), since date default null, until date default null)
-	returns table(name varchar(80), is_single boolean, released date, label varchar(80), copies int,
+create or replace function getAlbumsByBand (bandName varchar(80), since date default null, until date default null)
+	returns table(id int, name varchar(80), is_single boolean, released date, label varchar(80), copies int,
 		rec_from date, rec_to date, studios varchar(160), collab varchar)
-	language SQL STABLE
+	language SQL
+	stable
 as $$
 	with Collab as (select album_id, string_agg(B2.name,', ') as collab
 		from (Band as B1 join Album_Band as A1 using(band_id)) join (Band as B2 join Album_Band as A2 using(band_id)) using(album_id)
 		where B1.band_id<>B2.band_id and B1.name=bandName group by album_id)
-	select Album.name, Album.is_single, Album.release_date, Label.name, Album.copies_num,
+	select album_id, Album.name, Album.is_single, Album.release_date, Label.name, Album.copies_num,
 	 	Album.record_start_date, Album.record_end_date, concat_ws(', ', Album.studio1, Album.studio2),
 	 	Collab.collab
  	from Album left join Label using(label_id) join Album_Band using(album_id) join Band using(band_id) left join Collab using(album_id)
@@ -56,12 +59,13 @@ $$;
 -- Отбор можно производить по имени человека, по имени группы или по тому и другому сразу,
 -- или же можно вывести информацию по всем
 create or replace function getPersonalInfo (personName varchar(80) default null, bandName varchar(80) default null)
-	returns table(name varchar(80), aliases varchar, sex char(1), date_of_birth date, 
+	returns table(id, name varchar(80), aliases varchar, sex char(1), date_of_birth date, 
 		place_of_birth varchar, date_of_death date, place_of_death varchar)
-	language SQL STABLE
+	language SQL
+	stable
 as $$
 	with Aliases as (select person_id, string_agg(alias, ',') as aliases from Alias group by person_id)
-	select Person.name, Aliases.aliases, Person.sex, Person.birth_date, concat_ws(', ',P1.name,P1.addr,P1.country),
+	select person_id, Person.name, Aliases.aliases, Person.sex, Person.birth_date, concat_ws(', ',P1.name,P1.addr,P1.country),
 		Person.death_date, concat_ws(', ',P2.name,P2.addr,P2.country)
 	from Person left join Place as P1 on birth_place=P1.place_id
 		left join Place as P2 on death_place=P2.place_id left join Aliases using(person_id)
@@ -72,12 +76,12 @@ $$;
 -- Отбор можно производить по имени человека, по имени группы или по тому и другому сразу,
 -- или же можно вывести информацию по всем
 create or replace function getPersonalHistory (personName varchar(80) default null, bandName varchar(80) default null)
-	returns table (name varchar(80), band varchar(80), member_from date, member_until date, role varchar(80),
+	returns table (person_id int, name varchar(80), band varchar(80), member_from date, member_until date, role varchar(80),
 		role_from date, role_until date)
 	language SQL
-	STABLE
+	stable
 as $$
-	select Person.name, Band.name, Member.join_date, Member.leave_date, Role.name, Member_Role.start_date, Member_Role.end_date
+	select person_id, Person.name, Band.name, Member.join_date, Member.leave_date, Role.name, Member_Role.start_date, Member_Role.end_date
 	from Person left join Member using(person_id) join Band using(band_id)
 		left join Member_Role using(member_id) join Role using(role_id)
 	where (personName is null or Person.name = personName) and (bandName is null or person_id in
@@ -87,7 +91,7 @@ $$;
 create or replace function getBandLabels(bandName varchar(80), since date default null, until date default null)
 	returns table (label varchar(80))
 	language SQL
-	STABLE
+	stable
 as $$
 	select distinct(Label.name)
 	from Band join Album_Band using(band_id) join Album using(album_id) join Label using(label_id)
@@ -95,13 +99,13 @@ as $$
 $$;
 
 create or replace function getLabelAncestors (labelName varchar(80))
-	returns table (label varchar(80))
+	returns table (id int, label varchar(80))
 	language SQL
 	STABLE
 as $$
 	with recursive label_h(id,p) as (select label_id,parent from Label where name = labelName
 		UNION select label_id,parent from Label,label_h where label_id=p)
-	select name
+	select label_id, name
 	from Label join label_h on id=label_id;
 $$;
 
@@ -116,8 +120,7 @@ as $$
 		where Band.name=bandName and (albumName is null or Album.name=albumName);
 $$;
 
-create or replace function addSingle (compName varchar, released date, rec_from date, rec_to date,
-	labelName varchar, studio varchar, length int, styleName varchar, copies int, variadic bands varchar[])
+create or replace function addSingle (compName varchar, released date, rec_from date, rec_to date, labelName varchar, studio varchar, length int, styleName varchar, copies int, variadic bands varchar[])
 	returns void
 	language plpgsql
 	volatile
