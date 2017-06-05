6b17ff8be8d934b4d6008fee35c620a6b3901040
semitro <semitro_8@mail.ru>
2017-06-05 11:11:35 +0300

    Ждём допа

--- scripts/dfunctions.sql
+++ scripts/dfunctions.sql
@@ -6,7 +6,7 @@ drop function getPersonalInfo  (personName varchar(80), bandName varchar(80));
 drop function getPersonalHistory  (personName varchar(80), bandName varchar(80));
 drop function getBandLabels (bandName varchar(80), since date, until date);
 drop function getLabelAncestors  (labelName varchar(80));
-drop function getCompositionsOnAlbum  (bandName varchar, albumName varchar);
+drop function getCompositionsInAlbum  (bandName varchar, albumName varchar);
 drop function addSingle  (compName varchar, released date, rec_from date, rec_to date, labelName varchar, studio varchar, length int, styleName varchar, copies int, variadic bands varchar[]);
 drop function addCompositionsToAlbum  (bandName varchar, albumName varchar, variadic compositions varchar[]);
 drop function addPeopleToBand  (bandName varchar, variadic people varchar[]);
